f1b598d46d84cddb95effdad2cbb9c395bf0a70b
EC2 Default User <ec2-user@domU-12-31-39-00-56-25.compute-1.internal>
2011-04-20 06:55:29 +0000

    superclient80

--- proj1group/proj1/src/benchmark.sh
+++ proj1group/proj1/src/benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/162/proj1group/proj1/src
-ruby edu/berkeley/cs/cs162/benchmark_script.rb 100
-for i in {1..100}
+ruby edu/berkeley/cs/cs162/benchmark_script.rb 80
+for i in {1..80}
 do
 java edu/berkeley/cs/cs162/BenchmarkingChatClient < commands/command$i >screenouput$i 2> client$i &
 done
