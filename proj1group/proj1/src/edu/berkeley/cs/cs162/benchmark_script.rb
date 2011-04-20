30bcb300d608c4a8cc5d03d80320adc77d51cbb2
EC2 Default User <ec2-user@domU-12-31-39-00-56-25.compute-1.internal>
2011-04-20 06:40:36 +0000

    scripts and outputs

--- proj1group/proj1/src/edu/berkeley/cs/cs162/benchmark_script.rb
+++ proj1group/proj1/src/edu/berkeley/cs/cs162/benchmark_script.rb
@@ -5,11 +5,11 @@
 #Spawn Clients
 client_path = "edu/berkeley/cs/cs162/"
 client_filename = "BenchmarkingChatClient"
-
-for i in 1..1
-    `ruby generate_commands.rb #{i}`
+n=ARGV[0].to_i
+for i in 1..n
+  `ruby generate_commands.rb #{i} #{n}`
      command_file = File.open("commands/command#{i}", "r")
      command_string = command_file.read
-    #`java #{client_path}#{client_filename} << #{command_string}`
-    `cat << commands/command#{i}`
+  #`java #{client_path}#{client_filename} << #{command_string} >/dev/null 2> client#{i} &`
+  #`cat << commands/command#{i}`
 end
--- proj1group/proj1/src/generate_commands.rb
+++ proj1group/proj1/src/generate_commands.rb
@@ -3,8 +3,13 @@
 ## Generates commands for a BenchmarkingChatClient. Writes to a file in commands/ ##
 #
 string_id = ARGV[0]
-
-command_string = "" #ADD COMMAND STRING HERE
+value = ARGV[1].to_i
+command_string = "connect ec2-50-16-127-141.compute-1.amazonaws.com:8080\n"
+command_string += "adduser user#{string_id} password\n"
+command_string += "login user#{string_id} password\n"
+command_string += "sleep 20000\n"
+50.times { |i| command_string += "send user#{(string_id.to_i)%value + 1} #{i+1} \"hello user#{(string_id.to_i)%value + 1}\"\n" } 
+command_string += "print RTT\n" #ADD COMMAND STRING HERE
 
 File.open("commands/command#{string_id}", 'w') do |command_file|
 	command_file.puts(command_string)
