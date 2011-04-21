6066a84118b12f33a022cda018a52295eca79e8f
EC2 Default User <ec2-user@domU-12-31-39-0E-D5-22.compute-1.internal>
2011-04-21 02:03:19 +0000

    new ruby script for better generation of command files

---
+++ proj1group/proj1/src/benchmark_script.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+## Script that spawns multiple BenchmarkingChatClients to use in benchmarking. ##
+
+#Spawn Clients
+client_path = "edu/berkeley/cs/cs162/"
+client_filename = "BenchmarkingChatClient"
+n=ARGV[0].to_i
+for i in 1..n
+  `ruby generate_commands.rb #{i} #{n}`
+     command_file = File.open("commands/command#{i}", "r")
+     command_string = command_file.read
+#<<<<<<< HEAD
+  #`java #{client_path}#{client_filename} << #{command_string} >/dev/null 2> client#{i} &`
+  #`cat << commands/command#{i}`
+#=======
+#    `java #{client_path}#{client_filename} << #{command_string}`
+    #`cat << commands/command#{i}`
+#>>>>>>> a05c77b79aaba2e00464373a05e2a789046fd56b
+end
