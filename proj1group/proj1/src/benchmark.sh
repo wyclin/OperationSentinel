6fea7c743ae0acbf53c6f2e5e21e7ff28ebe2bb9
EC2 Default User <ec2-user@domU-12-31-39-0E-D5-22.compute-1.internal>
2011-04-21 02:03:10 +0000

    new ruby script for better generation of command files

--- proj1group/proj1/src/benchmark.sh
+++ proj1group/proj1/src/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/162/proj1group/proj1/src
-ruby edu/berkeley/cs/cs162/benchmark_script.rb 50
+ruby setup_commands.rb 100 51 100
 for i in {1..50}
 do
 java edu/berkeley/cs/cs162/BenchmarkingChatClient < commands/command$i >screenouput$i 2> client$i &
