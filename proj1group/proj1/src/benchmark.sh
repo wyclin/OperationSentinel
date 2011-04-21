ea360b212f2d9dbfa79ec9129f591c7165778d40
EC2 Default User <ec2-user@domU-12-31-39-09-48-C3.compute-1.internal>
2011-04-21 02:23:18 +0000

    improved bash script

--- proj1group/proj1/src/benchmark.sh
+++ proj1group/proj1/src/benchmark.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd ~/162/proj1group/proj1/src
-ruby setup_commands.rb 100 51 100
-for i in {1..50}
+ruby setup_commands.rb 10 1 10
+for i in {1..10}
 do
-java edu/berkeley/cs/cs162/BenchmarkingChatClient < commands/command$i >screenouput$i 2> client$i &
+java edu/berkeley/cs/cs162/BenchmarkingChatClient < commands/command$i >screenoutput$i 2> client$i &
 done
+
+sleep 300
+killall -9 java
+cat client* > results/superclient
+rm client* screenoutput*
